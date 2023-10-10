55663a553b009df132bdc79af17e77eea3f3acb7
wolverines2387 <ryanwt@umich.edu>
2023-10-09 21:24:16 -0600

    Update to asgi.py

--- airbnb_app/airbnb_app/asgi.py
+++ airbnb_app/airbnb_app/asgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+import django
+django.setup()
+
 import os
 
 from django.core.asgi import get_asgi_application
@@ -14,3 +17,17 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airbnb_app.settings")
 
 application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+#import auction.routers # Do I really need this?
+from channels.auth import AuthMiddlewareStack
+import recomender.routers
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            recomender.routers.websocket_urlpatterns
+        )
+    ),
+})
