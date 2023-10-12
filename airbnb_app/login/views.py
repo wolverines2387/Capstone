fb58fd24ca09206a4a4c42634d135a12ab4850d8
wolverines2387 <ryanwt@umich.edu>
2023-10-11 20:51:27 -0600

    views.py

--- airbnb_app/login/views.py
+++ airbnb_app/login/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/auction")
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirect to login page after logout
